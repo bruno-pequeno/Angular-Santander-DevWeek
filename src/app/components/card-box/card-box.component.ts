bcda0e4a307263aea0df7d458af7ada36462176e
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-26 16:50:00 -0300

    Dimanizing data

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
 
+export class CardBoxComponent {
+    accountData: AccountDataModel = {
+    name: "",
+    account: {
+        agency: "0000",
+        number: "00.000000-0"
+    },
+    card:{
+        limit: 0,
+        number: "0000"
+    }
+  };
 }

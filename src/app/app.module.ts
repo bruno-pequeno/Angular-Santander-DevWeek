f9cf4b8b59597536a9a744392992e5d92f914619
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-26 01:54:57 -0300

    Injecting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { SvgComponentsModule } from './modules/svg-components.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardBoxComponent } from './components/card-box/card-box.component';
+import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
+import { AmountBoxComponent } from './components/amount-box/amount-box.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+  HomeComponent,
+  HeaderComponent,
+  CardBoxComponent,
+  CarouselItemsComponent,
+  AmountBoxComponent,
 	],
 	imports: [
 		BrowserModule,

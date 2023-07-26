00904b647afbec2bb8276dc6699b96778014d151
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-26 18:18:46 -0300

    Adding Carousel

---
+++ src/app/services/features.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { enviroment } from '../environments/environments';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FeaturesService {
+
+	private apiUrl = enviroment.API_URL;
+
+	constructor(private http: HttpClient) { }
+
+	getFeature(): Observable<any> {
+		return this.http.get<any>(this.apiUrl);
+	}
+
+}

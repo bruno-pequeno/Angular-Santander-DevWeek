23c689acc260ff241603702d47e094097f47dd03
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-26 18:01:43 -0300

    Created 2nd Card

---
+++ src/app/services/accounts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { enviroment } from '../environments/environments';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class AccountsService {
+
+	private apiUrl = enviroment.API_URL;
+
+	constructor(private http: HttpClient) { }
+
+	getAccount(): Observable<any> {
+		return this.http.get<any>(this.apiUrl)
+	}
+}
